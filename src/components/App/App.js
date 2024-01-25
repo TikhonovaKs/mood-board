c83b9459d6556e57d7ddc2bc6d92dfaae95ee6d2
Kseniia Tikhonova <tikhonovakb@gmail.com>
2024-01-25 15:06:40 +0100

    test

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -6,6 +6,9 @@ function App() {
       <h1 className='app__title'>hello</h1>
       <div className='app__picturesList'></div> 
       <button className='app__addButton'></button>
+      <h1 className='app__title'>hello</h1>
+      <div className='app__picturesList'></div> 
+      <button className='app__addButton'></button>
     </div>
   );
 }
