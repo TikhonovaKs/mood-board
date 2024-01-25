d8f986168223543f94a75ba51708730f42c50d31
marianordqvist <marianordqvist@live.se>
2024-01-25 14:11:07 +0100

    create test component

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,11 +1,13 @@
-import './App.css';
+import "./App.css";
+import Test from "../Test/Test";
 
 function App() {
   return (
     <div className="app">
-      <h1 className='app__title'>hello</h1>
-      <div className='app__picturesList'></div> 
-      <button className='app__addButton'></button>
+      <h1 className="app__title">hello</h1>
+      <div className="app__picturesList"></div>
+      <button className="app__addButton"></button>
+      <Test />
     </div>
   );
 }
