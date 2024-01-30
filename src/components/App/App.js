56549be303095759259acd46599f7abfc674c26e
Kseniia Tikhonova <tikhonovakb@gmail.com>
2024-01-30 10:07:24 +0100

    feat: create search logic

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,9 +1,11 @@
 import './App.css';
 import SearchPage from '../../pages/SearchPage/SearchPage.js';
+import Header from '../Header/Header.js';
 
 function App() {
   return (
     <div className="app">
+      <Header />
       <SearchPage />
     </div>
   );
