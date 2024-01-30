cdf699f3c088dd26a9f0fd3d19f06706b8dc2615
Kseniia Tikhonova <tikhonovakb@gmail.com>
2024-01-30 15:10:10 +0100

    feat: add random tips

--- src/pages/SearchPage/SearchPage.js
+++ src/pages/SearchPage/SearchPage.js
@@ -1,33 +1,13 @@
 import React, { useState } from 'react';
-import photoApi from '../../utils/PhotoApi.js';
+import useFetch from '../../hooks/useFetch.js';
 import './SearchPage.css';
 import Search from '../../components/Search/Search.js';
 import Tips from '../../components/Tips/Tips.js';
 import CardList from '../../components/CardList/CardList.js';
-// import PhotoPopup from '../../components/PhotoPopup/PhotoPopup.js';
 
 function SearchPage() {
-  const [searchList, setSearchList] = useState([]);
-  const [keyword, setKeyword] = useState('');
-  // const keyword = 'nature';
-  
-  React.useEffect(() => {
-    photoApi
-      .searchPhoto(keyword)
-      .then((data) => {
-        console.log(data)
-        const photos = data.photos;
-        setSearchList(
-            photos.map((item) => ({
-            src: item.src.original,
-            alt: item.alt,
-          }))
-        );
-      })
-      .catch((err) => {
-        console.error(err);
-      });
-  }, [keyword]);
+ const [keyword, setKeyword] = useState('');
+ const { searchList } = useFetch(keyword);
   
   const handleSearch = (data) => {
     setKeyword(data);
