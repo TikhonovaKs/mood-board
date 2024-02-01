23d3f7bea92627603e0904d75b56f0bcb659940b
Kseniia Tikhonova <tikhonovakb@gmail.com>
2024-02-01 08:34:46 +0100

    add hover button for image

--- src/components/Card/Card.js
+++ src/components/Card/Card.js
@@ -1,18 +1,24 @@
-import React from "react";
-import "./Card.css";
+import React, { useState } from 'react';
+import { CiBookmarkPlus } from "react-icons/ci";
+import './Card.css';
 
 function Card({ card, onClick }) {
+  const [isHovered, setIsHovered] = useState(false);
+
   return (
-    <>
+    <div
+      onMouseEnter={() => setIsHovered(true)}
+      onMouseLeave={() => setIsHovered(false)}
+      style={{ position: 'relative', display: 'inline-block' }}
+    >
       <li>
-        <img
-          className="card"
-          src={card.src}
-          alt={card.alt}
-          onClick={() => onClick(card)}
-        />
+        <img className="card" src={card.src} alt={card.alt} onClick={() => onClick(card)} />
+        {isHovered && 
+        <button className="card__saveButton">
+          <CiBookmarkPlus />
+        </button>}
       </li>
-    </>
+    </div>
   );
 }
 
