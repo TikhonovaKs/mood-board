083a2be577a27b3ace0170243e97ac51d868e4e7
Kseniia Tikhonova <tikhonovakb@gmail.com>
2024-02-05 11:03:27 +0100

    feat: add grid for cardsList

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # Study project "Mood board"
 
+'react-icons/ci'
+react-transition-group" https://reactcommunity.org/react-transition-group/
--- src/components/CardList/CardList.js
+++ src/components/CardList/CardList.js
@@ -1,14 +1,14 @@
-import React, { useState } from "react";
-import "./CardList.css";
-import Card from "../Card/Card";
-import PhotoPopup from "../PhotoPopup/PhotoPopup";
+import React, { useState } from 'react';
+import './CardList.css';
+import Card from '../Card/Card';
+import PhotoPopup from '../PhotoPopup/PhotoPopup';
 
 function CardList({ cardsList }) {
   const [popupVisible, setPopupVisible] = useState(false);
   const [selectedCard, setSelectedCard] = useState(null);
 
   const onCardClick = (item) => {
-    console.log("Selected Card:", item);
+    console.log('Selected Card:', item);
     setSelectedCard(item);
     setPopupVisible(true);
   };
@@ -20,21 +20,19 @@ function CardList({ cardsList }) {
 
   return (
     <>
-      <ul className="cardList">
-        {cardsList &&
-          cardsList?.map((item, index) => (
-            <Card
-              key={index}
-              card={item}
-              layoutStyle="card card__layout1"
-              onClick={() => onCardClick(item)}
-            />
-          ))}
-      </ul>
-
+      <section className="cardList__wrapper">
+        <ul className="cardList">
+          {cardsList &&
+            cardsList?.map((item, index) => (
+              <li>
+                <Card key={index} card={item} layoutStyle="card card__layout1" onClick={() => onCardClick(item)} />
+              </li>
+            ))}
+        </ul>
+      </section>
       {popupVisible && (
         <PhotoPopup
-          className={popupVisible ? "popup_is-opened" : ""}
+          className={popupVisible ? 'popup_is-opened' : ''}
           selectedCard={selectedCard}
           onClose={closePopup}
         />
@@ -42,5 +40,4 @@ function CardList({ cardsList }) {
     </>
   );
 }
-
 export default CardList;
